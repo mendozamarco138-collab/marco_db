7f3c28714c5c90894f19eb73f306beacd705fdba
mendozamarco138-collab <mendozamarco138@gmail.com>
2026-04-01 11:11:17 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,18 @@
-self.addEventListener('fetch', function(event) {
-  // Driver lang ito, kahit walang karga, basta nandito siya.
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('marco-db-v1').then((cache) => {
+      return cache.addAll([
+        './index.html',
+        './manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
